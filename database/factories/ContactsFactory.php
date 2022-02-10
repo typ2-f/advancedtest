4c116427ddb92dd9e6f93897ef70ac11545afcd7
Kento Okamori <myg.min3btn@gmail.com>
2022-02-11 05:49:47 +0900

    make factory

---
+++ database/factories/ContactsFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Contact::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'fullname' => $this->faker->name,
+            'gender' => $this->faker->numberBetween(1, 2),
+            'email' => $this->faker->safeEmail(),
+            'postcode'=> $this->faker->postcode,
+            'address'=> $this->faker->streetAddress(),
+            'building_name'=> $this->faker->company(),
+            'opinion' => $this->faker->sentence(),
+        ];
+    }
+}
